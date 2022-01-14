1879e757de97c0af1ddba52975f787495f613a23
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-01-14 16:04:49 -0300

    build(Inputs): recebendo props do Login

--- src/components/form/Inputs.js
+++ src/components/form/Inputs.js
@@ -1,20 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Inputs extends React.Component {
   render() {
+    const { email, name, handleChange } = this.props;
     return (
       <div>
         <label htmlFor="name">
           Nome:
-          <input type="text" data-testid="input-player-name" />
+          <input
+            type="text"
+            data-testid="input-player-name"
+            name="name"
+            value={ name }
+            onChange={ handleChange }
+          />
         </label>
         <label htmlFor="email">
           Email:
-          <input type="email" data-testid="input-gravatar-email" />
+          <input
+            type="email"
+            data-testid="input-gravatar-email"
+            name="email"
+            value={ email }
+            onChange={ handleChange }
+          />
         </label>
       </div>
     );
   }
 }
+
+Inputs.propTypes = {
+  email: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
 
 export default Inputs;

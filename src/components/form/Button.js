ecdb497220f6458b1d5f6ec17b47f26a72002abb
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-01-14 16:03:57 -0300

    build(Button): recebendo props isDisable do Login

--- src/components/form/Button.js
+++ src/components/form/Button.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Button extends React.Component {
   render() {
+    const { isDisable } = this.props;
     return (
       <div>
-        <button type="button" data-testid="btn-play">Play</button>
+        <button
+          type="button"
+          data-testid="btn-play"
+          disabled={ isDisable }
+        >
+          Play
+        </button>
       </div>
     );
   }
 }
+
+Button.propTypes = {
+  isDisable: PropTypes.bool.isRequired,
+};
 
 export default Button;

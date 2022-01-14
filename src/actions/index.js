ed5c12c48ce2d2099865353685e204c5bd00a9f3
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-01-14 16:47:07 -0300

    build(index): Criando a função de retorno da api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import requestTokenAPI from '../services/requestToken';
+
 export const USER_INFO = 'USER_INFO';
 export const TOKEN_GAME = 'TOKEN_GAME';
 
@@ -10,3 +12,8 @@ export const actionToken = (payload) => ({
   type: TOKEN_GAME,
   payload,
 });
+
+export const requestApiToken = () => (dispatch) => {
+  requestTokenAPI()
+    .then((token) => dispatch(actionToken(token)));
+};

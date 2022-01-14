4af163249fb5b4b4f4622254ed940822ce61205c
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-01-14 16:06:10 -0300

    build(Login): criação das funções e passando as infos via props

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Inputs from '../components/form/Inputs';
 import Button from '../components/form/Button';
-// import PropTypes from 'prop-types';
 
 class Login extends React.Component {
   constructor() {
@@ -9,14 +8,39 @@ class Login extends React.Component {
     this.state = {
       email: '',
       name: '',
+      isDisable: true,
     };
   }
 
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value }, () => this.enableBtn());
+  }
+
+  enableBtn = () => {
+    const { email, name } = this.state;
+    const zero = 0;
+    if (email.length > zero && name.length > zero) {
+      this.setState({
+        isDisable: false,
+      });
+    } else {
+      this.setState({
+        isDisable: true,
+      });
+    }
+  }
+
   render() {
+    const { handleChange } = this;
+    const { email, name, isDisable } = this.state;
     return (
       <div>
-        <Inputs />
-        <Button />
+        <Inputs
+          handleChange={ handleChange }
+          email={ email }
+          name={ name }
+        />
+        <Button isDisable={ isDisable } />
       </div>
     );
   }

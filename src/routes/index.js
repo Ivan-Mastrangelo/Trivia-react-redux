8d718c5d8b417728410bef2e2b8bfa41308a9501
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-01-17 22:23:56 -0300

    build(Index): removendo chamada de funcao e criando rota

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from '../pages/Login';
-import requestTokenAPI from '../services/requestToken';
+import Game from '../pages/Game';
 
 class Index extends Component {
-  componentDidMount() {
-    requestTokenAPI();
-  }
-
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route exact path="/game" component={ Game } />
       </Switch>
     );
   }

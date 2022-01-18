aacfb8a59462f1fb6298c9adc8e5d630cb600211
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-01-17 22:24:48 -0300

    build(Token): removendo o console log do reducer

--- src/reducer/token.js
+++ src/reducer/token.js
@@ -3,18 +3,11 @@ import { TOKEN_GAME } from '../actions';
 const INITIAL_STATE = '';
 
 const token = (state = INITIAL_STATE, action) => {
-  console.log(action);
-  // const getToken = {
-  // [TOKEN_GAME]: {
-  // return { action.token.token }
-  // },
-  // };
-  // const verify = getToken[action.type];
-  // if (verify) return verify;
-  // return state;
   switch (action.type) {
-  case TOKEN_GAME: return action.token.token;
-  default: return state;
+  case TOKEN_GAME:
+    return action.token.token;
+  default:
+    return state;
   }
 };
 

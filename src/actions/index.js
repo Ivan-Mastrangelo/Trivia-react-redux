2636d936b639d212278ae7b3d2358b8f235d745c
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-01-18 16:39:12 -0300

    build(Index): criando a action do game e solitando infos da api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,9 @@
 import requestTokenAPI from '../services/requestToken';
+import requestGameAPI from '../services/requestApiGame';
 
 export const USER_INFO = 'USER_INFO';
 export const TOKEN_GAME = 'TOKEN_GAME';
+export const TOKEN_QUESTIONS = 'TOKEN_QUESTIONS';
 
 export const actionUser = (payload) => ({
   type: USER_INFO,
@@ -13,7 +15,24 @@ export const actionToken = (token) => ({
   token,
 });
 
+export const actionGame = (token) => ({
+  type: TOKEN_QUESTIONS,
+  token,
+});
+
 export const requestApiToken = () => (dispatch) => {
   requestTokenAPI()
-    .then((token) => dispatch(actionToken(token)));
+    .then((token) => {
+      localStorage.setItem('token', token.token);
+      dispatch(actionToken(token));
+    });
+};
+
+export const requestApiGame = () => (dispatch) => {
+  const getToken = localStorage.getItem('token');
+  requestGameAPI(getToken)
+    .then((game) => {
+      localStorage.setItem('game', game.results);
+      dispatch(actionGame(game));
+    });
 };

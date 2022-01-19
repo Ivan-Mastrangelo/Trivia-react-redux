19a2f6fd025996afedd4bc286e58e7b628eb608c
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-01-19 19:37:22 -0300

    build(Heading): pegando results do estado

--- src/components/game/components/Heading.js
+++ src/components/game/components/Heading.js
@@ -1,23 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Heading extends Component {
   render() {
     const { getResults } = this.props;
     return (
-      <>
-        <h1
-          data-testid="question-category"
-        >
-          {getResults[0].category}
-        </h1>
-        <h5
-          data-testid="question-text"
-        >
-          {getResults[0].question}
-        </h5>
-      </>
+      getResults.length > 0 && (
+        <div>
+          <h1
+            data-testid="question-category"
+          >
+            {getResults[0].category}
+          </h1>
+          <h5
+            data-testid="question-text"
+          >
+            {getResults[0].question}
+          </h5>
+        </div>
+      )
     );
   }
 }
 
-export default Heading;
+const mapStateToProps = (state) => ({
+  getResults: state.game.results,
+});
+
+export default connect(mapStateToProps)(Heading);

5cf17405d4041edefc39ca26859801a0f8d7d3de
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-01-19 17:29:23 -0300

    build(Game): criando renderizacao condicionada e chamando o componente bodygame

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import BodyGame from '../components/game/BodyGame';
 import Header from '../components/game/Header';
 
 class Game extends Component {
   render() {
+    const { getResults, loading } = this.props;
+    console.log(getResults);
     return (
       <div>
-        <Header />
+        {!loading && (
+          <>
+            <Header />
+            <BodyGame />
+          </>
+        )}
       </div>
     );
   }
 }
 
-export default Game;
+const mapStateToProps = (state) => ({
+  loading: state.game.loading,
+});
+
+export default connect(mapStateToProps)(Game);

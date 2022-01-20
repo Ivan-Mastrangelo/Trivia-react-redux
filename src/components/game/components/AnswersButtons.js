85689648d4e2889ac1042698affd1afc03185518
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-01-19 21:39:15 -0300

    build(AnswersButtons): criando funcoes, colocando botoes para renderizar e requisito 6 finalizado

--- src/components/game/components/AnswersButtons.js
+++ src/components/game/components/AnswersButtons.js
@@ -6,6 +6,7 @@ class AnswersButtons extends Component {
     super();
     this.state = {
       arrayAnswers: [],
+      color: '',
     };
   }
 
@@ -13,42 +14,72 @@ class AnswersButtons extends Component {
     this.aleatoryAnswers();
   }
 
+  clickButton = ({ target }) => {
+    const { value } = target;
+    if (value === 'correct') {
+      this.setState({
+        color: '3px solid rgb(6, 240, 15)',
+      });
+    }
+    if (value === 'incorret') {
+      this.setState({
+        color: '3px solid rgb(255, 0, 0)',
+      });
+    }
+  }
+
   aleatoryAnswers() {
     const { getResults } = this.props;
-    const correctAnswer = getResults[0].correct_answer;
-    const arrayIncorrectAnswers = getResults[0].incorrect_answers;
-    const correct_answers = { correctAnswer };
-    const incorrectAnswers = arrayIncorrectAnswers.map((answer) => ({
-      answer,
-    }));
-
-    console.log(incorrectAnswers);
-    this.setState({
-      arrayAnswers: [correct_answers, incorrectAnswers],
-    });
+    if (getResults.length > 0) {
+      const correctAnswer = getResults[0].correct_answer;
+      const arrayIncorrectAnswers = getResults[0].incorrect_answers;
+      const correct_answers = { correctAnswer };
+      const incorrectAnswers = arrayIncorrectAnswers.map((incorrectAnswers) => ({
+        incorrectAnswers,
+      }));
+      // https://flaviocopes.com/how-to-shuffle-array-javascript/
+      const arrWithAnswers = [correct_answers, ...incorrectAnswers];
+      const magicNumber = 0.5;
+      const newArrAleatory = arrWithAnswers.sort(() => Math.random() - magicNumber);
+      this.setState({
+        arrayAnswers: newArrAleatory,
+      });
+    }
   }
 
   render() {
-    const { arrayAnswers, getResults } = this.props;
+    const { color, arrayAnswers } = this.state;
     return (
       <div
         data-testid="answer-options"
       >
-        <button
-          type="button"
-          data-testid="correct-answer"
-        >
-          {getResults[0].correct_answer}
-        </button>
-        {getResults[0].incorrect_answers.map((answer, index) => (
-          <button
-            key={ index }
-            type="button"
-            data-testid={ `wrong-answer-${index}` }
-          >
-            {answer}
-          </button>
-        ))}
+        {arrayAnswers.map(({ correctAnswer, incorrectAnswers }, index) => {
+          if (correctAnswer) {
+            return (
+              <button
+                type="button"
+                value="correct"
+                data-testid="correct-answer"
+                onClick={ (e) => this.clickButton(e) }
+                style={{ border: color }}
+              >
+                {correctAnswer}
+              </button>
+            );
+          }
+          return (
+            <button
+              key={ incorrectAnswers }
+              type="button"
+              value="incorrect"
+              data-testid={ `wrong-answer-${index}` }
+              onClick={ (e) => this.clickButton(e) }
+              style={{ border: color }}
+            >
+              {incorrectAnswers}
+            </button>
+          );
+        })}
       </div>
     );
   }

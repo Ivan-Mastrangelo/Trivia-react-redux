07ca1149e4ab2f1e7f86ae3ddc86423bf97485c9
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-01-22 19:19:34 -0300

    build(NextButton): chamando a funcao via props

--- src/components/game/components/NextButton.js
+++ src/components/game/components/NextButton.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 
 class NextButton extends Component {
   render() {
-    const { statusButton } = this.props;
+    const { statusButton, handleClickIndex } = this.props;
+    console.log(index);
     return (
       <button
         type="button"
         data-testid="btn-next"
         style={ { visibility: statusButton } }
+        onClick={ () => handleClickIndex() }
       >
         Next
       </button>
@@ -18,6 +20,7 @@ class NextButton extends Component {
 
 NextButton.propTypes = {
   statusButton: PropTypes.string.isRequired,
+  handleClickIndex: PropTypes.func.isRequired,
 };
 
 export default NextButton;

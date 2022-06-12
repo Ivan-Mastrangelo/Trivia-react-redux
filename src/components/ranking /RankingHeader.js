4167c816b0fa16cdbaeefc05e27a158b2644e3b3
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-06-12 20:18:31 -0300

    sincronizar commits

---
+++ src/components/ranking /RankingHeader.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class RankingHeader extends Component {
+  render() {
+    return (
+      <div>
+        <h5>
+          teste
+        </h5>
+      </div>
+    );
+  }
+}
+
+export default RankingHeader;
